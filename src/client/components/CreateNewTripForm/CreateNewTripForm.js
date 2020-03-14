6e88b71fc5eeb05bd7cde5b1ad1e16abf3a1a7c3
Supran Sengupta <supransengupta@gmail.com>
2020-03-14 18:01:51 +0530

    added create new trip form

--- src/client/components/CreateNewTripForm/CreateNewTripForm.js
+++ src/client/components/CreateNewTripForm/CreateNewTripForm.js
@@ -1,6 +1,6 @@
 
 
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useCallback } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 // import { TextField } from 'formik-material-ui';
@@ -9,91 +9,99 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 // import FormTextField from '../common/FormTextField/FormTextField';
 import {
-    TimePicker,
-    DatePicker,
-    DateTimePicker,
+  TimePicker,
+  DatePicker,
+  DateTimePicker,
 } from 'formik-material-ui-pickers';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 
 import LocatioSearchInput from '../common/LocationSearchInput'
+import styles from './CreateNewTripForm.style';
 
 
 const getPlaceObjectToAdd = () => ({
-    name: '',
-    startDate: new Date(),
-    endDate: new Date()
+  place: {},
+  startDate: new Date(),
+  endDate: new Date()
 })
 
+const updatePlaceObject = (newPoi, place) => {
+  return {
+    place: newPoi,
+    ...place
+  }
+}
+
 const CreateNewTripForm = (props) => {
-    return (
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Formik initialValues={{
-                tripTitle: '',
-                places: [
-                   getPlaceObjectToAdd()
-                ]
-            }}
-                onSubmit={(values, isSubmitting) => {
-                    console.log("Submitting value", values)
-                }}
-            >
-                {({ values, submitForm, isSubmitting }) => (
-                    <Form>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <Field name="tripTitle">
-                                    {
-                                        (field, form, meta) => (
-                                            <TextField {...field} variant='outlined' fullWidth={true} placeholder="Enter trip name" />
-                                        )
-                                    }
-                                </Field>
+  const classes = styles();
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Formik initialValues={{
+        tripTitle: '',
+        places: [getPlaceObjectToAdd()]
+      }}
+        onSubmit={(values, isSubmitting) => {
+          console.log("Submitting value", values)
+        }}
+      >
+        {({ values, submitForm, isSubmitting }) => (
+          <Form>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Field name="tripTitle">
+                  {
+                    (field, form, meta) => (
+                      <TextField {...field} variant='outlined' fullWidth={true} placeholder="Enter trip name" />
+                    )
+                  }
+                </Field>
+              </Grid>
+              <FieldArray
+                name="places"
+                render={
+                  arrayHelpers => (
+                    <>
+                      {
+                        values.places && values.places.map((place, index) => (
+                          <Grid container spacing={3} key= {index} className={classes.grid}>
+                            <Grid item xs={6}>
+                              <Field component={LocatioSearchInput} name="place" label="Place" selectPlace={(v) => arrayHelpers.replace(index, updatePlaceObject(v, place))} />
                             </Grid>
-                            <FieldArray
-                                name="places"
-                                render={
-                                    arrayHelpers => (
-                                        <>
-                                            {
-                                                values.places && values.places.map((place, index) => (
-                                                    <>
-                                                        <Grid item xs={6}>
-                                                            <Field component={LocatioSearchInput} name="place" label="Place" value={place.name} />
-                                                        </Grid>
-                                                        <Grid item xs={2}>
-                                                            <Field component={DatePicker} name="date" label="From Date" value={place.startDate} />
-                                                        </Grid>
-                                                        <Grid item xs={2}>
-                                                            <Field component={DatePicker} name="date" label="Till Date" value={place.endDate} />
-                                                        </Grid>
-                                                        <Grid item xs={1}>
-                                                                <Button variant="contained" color="primary" onClick={() => arrayHelpers.insert(index , getPlaceObjectToAdd())}>Add</Button>
-                                                        </Grid>
-                                                        <Grid item xs={1}>
-                                                                <Button variant="contained" color="secondary" disabled={values.places.length === 1} onClick={() => arrayHelpers.remove(index)}>Remove</Button>
-                                                        </Grid>
-                                                   </>
-                                                ))
-                                            }
-                                        </>
-                                    )
-                                }
-                            />
-                        <Grid container justify="start" spacing={3} style = {{ marginTop:'20', marginLeft:'10'}}>
-                            <Grid xs={1}>
-                                <Button type="submit" variant="contained" color="primary">Create</Button>
+                            <Grid item xs={2}>
+                              <Field component={DatePicker} name="date" label="From Date" value={place.startDate} />
                             </Grid>
-                            <Grid xs ={1}>
-                                <Button variant="contained" color="secondary">Cancel</Button>
+                            <Grid item xs={2}>
+                              <Field component={DatePicker} name="date" label="Till Date" value={place.endDate} />
                             </Grid>
-                        </Grid>
-                        </Grid>
-                    </Form>
-                )}
-            </Formik>
-        </MuiPickersUtilsProvider>
-    );
+                            <Grid item xs={1}>
+                              <Button variant="contained" color="primary" onClick={() => arrayHelpers.insert(index, getPlaceObjectToAdd())}>Add</Button>
+                            </Grid>
+                            <Grid item xs={1}>
+                              <Button variant="contained" color="secondary" disabled={values.places.length === 1} onClick={() => arrayHelpers.remove(index)}>Remove</Button>
+                            </Grid>
+                          </Grid>
+                        ))
+                      }
+                    </>
+                  )
+                }
+              />
+              <Grid container spacing={3} className={classes.grid}>
+                <Grid item xs={1}>
+                  <Button type="submit" variant="contained" color="primary">Create</Button>
+                </Grid>
+                <Grid item xs={1}>
+                  <Button variant="contained" color="secondary">Cancel</Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Form>
+        )}
+      </Formik>
+    </MuiPickersUtilsProvider>
+  );
 }
 
 export default CreateNewTripForm;
--- src/client/components/common/LocationSearchInput/LocationSearchInput.js
+++ src/client/components/common/LocationSearchInput/LocationSearchInput.js
@@ -1,36 +1,76 @@
-import React, { useState, useEffect , useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import TextField from '@material-ui/core/TextField';
 import debounce from 'lodash/debounce';
 import get from 'lodash/get';
 import { ajax } from 'rxjs/ajax'
-import { of  } from 'rxjs';
-import { map ,  catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Label from '@material-ui/core/InputLabel'
 
-const LocationSearchInput = ({ field , form: {touched , errors } , ...props}) => {
-    const [ result , setResult ] = useState([]);;
-    
-    const callSearchApi = ( query ) => {
-      return ajax.getJSON(`/api/search?place=${query}`).pipe(
-           map(response => response.message.features.map(res => res.text)),
-           catchError(error => of(error))
-       );
+let debouncedSearchApi;
+
+const LocationSearchInput = ({ field, form: { touched, errors }, ...props }) => {
+    const [result, setResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [open, setOpen] = useState(false);
+    const callSearchApi = (query) => {
+        return ajax.getJSON(`/api/search?place=${query}`).pipe(
+            map(response => response.message.features.map(res => ({
+                id: res.id,
+                short_name: res.text,
+                long_name:res.place_name,
+                geometry: res.geometry
+            }))),
+            catchError(error => of(error))
+        );
     };
 
-    const invokeSearchWithDebounce = debounce(() => {
-        const value = get(field , "value" , null);
-        value && value !== '' && callSearchApi(field.value).subscribe(res => {
-            console.log("response from seaarch ==> ",res);
-            setResult(res);
-        })
-    },1000);
+    const handleOnChange = useCallback((e , v) => {
+        props.selectPlace(v);
+    });
 
     useEffect(() => {
-        invokeSearchWithDebounce();
-    } , [field.value])
+        debouncedSearchApi = debounce((value) => {
+            if (value && value !== '') {
+                setLoading(true);
+                callSearchApi(value).subscribe(result => {
+                    setResult(result);
+                    setLoading(false);
+                });
+            }
+        }, 500);
+    }, []);
 
-    return <div>
-            <TextField {...field } {...props} value={field.value} fullWidth placeholder="Enter name of the place" />
-        </div>
+    useEffect(() => {
+       debouncedSearchApi(field.value);
+    }, [field.value])
+
+    return <Autocomplete
+        id='location-search-input'
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        getOptionSelected={(option, value) => option.id === value.id}
+        getOptionLabel={option => option.long_name}
+        options={result}
+        loading={loading}
+        onChange={handleOnChange}
+        renderInput={params => (
+            <TextField {...params}
+                {...field}
+                label={props.label}
+                InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                        <React.Fragment>
+                            {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                            {params.InputProps.endAdornment}
+                        </React.Fragment>
+                    ),
+                }} />
+        )} />
 }
 
 export default LocationSearchInput;
